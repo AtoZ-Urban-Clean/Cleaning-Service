daeb8131fea11ef0f96d277df5541daa03e9f7f5
AtoZ-Urban-Clean <yuvaoppo2025@gmail.com>
2026-02-03 09:32:27 +0530

    cart.js

--- js/cart.js
+++ js/cart.js
@@ -1,22 +1,18 @@
-/* GLOBAL CART */
 let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
-function addToCart(name, price){
-  cart.push({ name, price });
+function addToBag(name, price){
+  cart.push({name, price});
   localStorage.setItem("cart", JSON.stringify(cart));
+
   updateBagCount();
-  alert("Service added to Bag");
+  alert(name + " added to bag");
 }
 
 function updateBagCount(){
   const bag = document.querySelector(".bag-btn");
   if(bag){
-    bag.innerHTML = `BAG (${cart.length})`;
+    bag.innerText = "BAG (" + cart.length + ")";
   }
-}
-
-function getCartTotal(){
-  return cart.reduce((t,i)=>t + Number(i.price), 0);
 }
 
 updateBagCount();
